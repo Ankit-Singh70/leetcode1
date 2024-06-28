acf9e284c4abb9ee82392dba13f48d28399ae281
Ankit singh <144905398+Ankit-Singh70@users.noreply.github.com>
2024-06-28 19:54:15 +0530

    Time: 2 ms (42.66%), Space: 9.8 MB (47.03%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,16 @@
+
+  class Solution {
+public:
+void Preorder(TreeNode *root,vector<int>&ans){
+    if(root==NULL) return;
+    ans.push_back(root->val);
+    Preorder(root->left,ans);
+    Preorder(root->right,ans);
+}
+    vector<int> preorderTraversal(TreeNode* root) {
+       vector<int>ans;
+       Preorder(root,ans);
+       return ans;
+        
+    }
+};
